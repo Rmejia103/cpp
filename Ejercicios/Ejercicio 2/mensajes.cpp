3321bfbcc8bec11ad457dc1d9c73ea2fa0f952ca
Ruben Mejia <ruben.mejia@unah.hn>
2022-05-21 21:01:37 -0600

    Creando Ejercicio 2

---
+++ Ejercicios/Ejercicio 2/mensajes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string nombre;
+
+    cout << " Ingrese su nomnre : ";
+    cin >> nombre;
+
+    cout <<endl;
+    cout << " Hola mi nombre es : " << nombre <<endl;
+    cout <<endl;
+    
+    return 0;
+}
